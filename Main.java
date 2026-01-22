192057c055ab02ca3d54a6c1b7d52755080432f7
SohailaN12 <sohaila.noori2027@gbwl.org>
2026-01-22 19:24:53 +0000

    added more code

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
 
    public static void main(String []args) {
+
+         SkiingTrip t = new SkiingTrip("Sohaila", "LeviFinland", ""); 
    }
 }
--- SkiingTrip.java
+++ SkiingTrip.java
@@ -4,7 +4,7 @@ private static in processFee = 100; // class variables
 private String travelerName; // provided by client  
 private String destination; // provided by client 
 private int durationalInDays; // provided by client 
-private boolean isCabins; // provided by client 
+private boolean isSaariselka; // provided by client 
 private double totalCost; // calculated the totalCost 
 
 // 2. CONSTRUCTORS (defualt + parameterized)
@@ -12,11 +12,14 @@ public SkiingTrip() {
     this.travelerName = "defaultClient";
     this.destination = "Northern Lights Village Saariselkä-Downhill Skiing and Aurora Après Ski";
     this.durationalInDays = 7;
-    this.isSaariselkä = ture;
+    this.isSaariselka = ture;
     this.totalCost = processFee *  durationalInDays; // calculation
 }
-public SkiingTrip(String name, String destination, int duration, boolean Saariselkä) {
+public SkiingTrip(String name, String destination, int duration, boolean ture) {
     this.travelerName = name; // set instance variable to client-provieded value 
+    this.destination = destination;
+    this.durationalInDays = duration;
+    this.isSaariselka = ture; 
     this.totalCost = processFee * this.durationalInDays; // calculation 
      
 }
